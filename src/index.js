01d18dcbb639f36b757c473f08d81c3c7a4e3a85
aiakimov <akim.boroda@gmail.com>
2022-04-21 19:53:27 +0300

    Task-6 completed

--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import Task from "./Task.js";
+import Checkbox from "./Checkbox.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ul className="task-list">
-      <Task number={1} text="Нaписать список" done={true}></Task>
-      <Task number={2} text="Зачеркнуть первый пункт" done={false}></Task>
-      <Task
-        number={3}
-        text="Сделать вид, что очень занят работой"
-        done={false}
-      ></Task>
-      <Task number={4} text="" done={false}></Task>
-      <Task number={5} text="Придумать интересный пункт 4" done={false}></Task>
-    </ul>
+    <Checkbox />
   </React.StrictMode>
 );
