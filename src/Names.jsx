9773c45429619d4975085e06006dce8d7c8682e0
aiakimov <akim.boroda@gmail.com>
2022-04-28 18:15:34 +0300

    Task-19

---
+++ src/Names.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Names.css";
+import Users from "./data.json";
+
+function Names() {
+  return (
+    <div className="wrapper">
+      {Users.map((el) => {
+        return (
+          <div key={el._id} className="user-card">
+            <h3>{el.name.first}</h3>
+            <h4>{el.name.last}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Names;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import ItalicFonts from "./ItalicFonts.jsx";
-
+import Names from "./Names.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    <ItalicFonts toolTip="Cursive" />
-
+    <Names />
   </React.StrictMode>
 );
