cb4b5b0c5eb3e3e4e37d2687d057db10ff310055
aiakimov <akim.boroda@gmail.com>
2022-04-28 17:29:54 +0300

    Task-17

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import ItalicFonts from "./ItalicFonts.jsx";
-
+import SayHi from "./SayHi.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    <ItalicFonts toolTip="Cursive" />
-
+    <SayHi />
   </React.StrictMode>
 );
