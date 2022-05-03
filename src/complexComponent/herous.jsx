05a957ad191b9e32b662c41d13defb8a2be3b28e
aiakimov <akim.boroda@gmail.com>
2022-05-03 09:20:47 +0300

    Task-22

---
+++ src/complexComponent/herous.jsx
@@ -0,0 +1,618 @@
+import abbadon from "../Abaddon_icon.webp";
+import alchemist from "./img/Alchemist_icon.webp";
+import ancient_apparition from "./img/Ancient_Apparition_icon.webp";
+import Anti_Mage from "./img/Anti-Mage_icon.webp";
+import Arc_Warden from "./img/Arc_Warden_icon.webp";
+import Axe from "./img/Axe_icon.webp";
+import Bane from "./img/Bane_icon.webp";
+import Batrider from "./img/Batrider_icon.webp";
+import Beastmaster from "./img/Beastmaster_icon.webp";
+import Bloodseeker from "./img/Bloodseeker_icon.webp";
+import Bounty_Hunter from "./img/Bounty_Hunter_icon.webp";
+import Brewmaster from "./img/Brewmaster_icon.webp";
+import Bristleback from "./img/Bristleback_icon.webp";
+import Broodmother from "./img/Broodmother_icon.webp";
+import Centaur_Warrunner from "./img/Centaur_Warrunner_icon.webp";
+import Chaos_Knight from "./img/Chaos_Knight_icon.webp";
+import Chen from "./img/Chen_icon.webp";
+import Clinkz from "./img/Clinkz_icon.webp";
+import Clockwerk from "./img/Clockwerk_icon.webp";
+import Crystal_Maiden from "./img/Crystal_Maiden_icon.webp";
+import Dark_Seer from "./img/Dark_Seer_icon.webp";
+import Dark_Willow from "./img/Dark_Willow_icon.webp";
+import Dawnbreaker from "./img/Dawnbreaker_icon.webp";
+import Dazzle from "./img/Dazzle_icon.webp";
+import Death_Prophet from "./img/Death_Prophet_icon.webp";
+import Disruptor from "./img/Disruptor_icon.webp";
+import Doom from "./img/Doom_icon.webp";
+import Dragon_Knight from "./img/Dragon_Knight_icon.webp";
+import Drow_Ranger from "./img/Drow_Ranger_icon.webp";
+import Earth_Spirit from "./img/Earth_Spirit_icon.webp";
+import Earthshaker from "./img/Earthshaker_icon.webp";
+import Elder_Titan from "./img/Elder_Titan_icon.webp";
+import Ember_Spirit from "./img/Ember_Spirit_icon.webp";
+import Enchantress from "./img/Enchantress_icon.webp";
+import Enigma from "./img/Enigma_icon.webp";
+import Faceless_Void from "./img/Faceless_Void_icon.webp";
+import Grimstroke from "./img/Grimstroke_icon.webp";
+import Gyrocopter from "./img/Gyrocopter_icon.webp";
+import Hoodwink from "./img/Hoodwink_icon.webp";
+import Huskar from "./img/Huskar_icon.webp";
+import Invoker from "./img/Invoker_icon.webp";
+import Io from "./img/Io_icon.webp";
+import Jakiro from "./img/Jakiro_icon.webp";
+import Juggernaut from "./img/Juggernaut_icon.webp";
+import Keeper_of_the_Light from "./img/Keeper_of_the_Light_icon.webp";
+import Kunkka from "./img/Kunkka_icon.webp";
+import Legion_Commander from "./img/Legion_Commander_icon.webp";
+import Leshrac from "./img/Leshrac_icon.webp";
+import Lich from "./img/Lich_icon.webp";
+import Lifestealer from "./img/Lifestealer_icon.webp";
+import Lina from "./img/Lina_icon.webp";
+import Lion from "./img/Lion_icon.webp";
+import Lone_Druid from "./img/Lone_Druid_icon.webp";
+import Luna from "./img/Luna_icon.webp";
+import Lycan from "./img/Lycan_icon.webp";
+import Magnus from "./img/Magnus_icon.webp";
+import Marci from "./img/Marci_icon.webp";
+import Mars from "./img/Mars_icon.webp";
+import Medusa from "./img/Medusa_icon.webp";
+import Meepo from "./img/Meepo_icon.webp";
+import Mirana from "./img/Mirana_icon.webp";
+import Monkey_King from "./img/Monkey_King_icon.webp";
+import Morphling from "./img/Morphling_icon.webp";
+import Naga_Siren from "./img/Naga_Siren_icon.webp";
+import Natures_Prophet from "./img/Natures_Prophet_icon.webp";
+import Necrophos from "./img/Necrophos_icon.webp";
+import Night_Stalker from "./img/Night_Stalker_icon.webp";
+import Nyx_Assassin from "./img/Nyx_Assassin_icon.webp";
+import Ogre_Magi from "./img/Ogre_Magi_icon.webp";
+import Omniknight from "./img/Omniknight_icon.webp";
+import Oracle from "./img/Oracle_icon.webp";
+import Outworld_Destroyer from "./img/Outworld_Destroyer_icon.webp";
+import Phantom_Assassin from "./img/Phantom_Assassin_icon.webp";
+import Phantom_Lancer from "./img/Phantom_Lancer_icon.webp";
+import Phoenix from "./img/Phoenix_icon.webp";
+import Primal_Beast from "./img/Primal_Beast_icon.webp";
+import Puck from "./img/Puck_icon.webp";
+import Pudge from "./img/Pudge_icon.webp";
+import Pugna from "./img/Pugna_icon.webp";
+import Pangolier from "./img/Pangolier_icon.webp";
+import Queen_of_Pain from "./img/Queen_of_Pain_icon.webp";
+import Razor from "./img/Razor_icon.webp";
+import Riki from "./img/Riki_icon.webp";
+import Rubick from "./img/Rubick_icon.webp";
+import Sand_King from "./img/Sand_King_icon.webp";
+import Shadow_Demon from "./img/Shadow_Demon_icon.webp";
+import Shadow_Fiend from "./img/Shadow_Fiend_icon.webp";
+import Shadow_Shaman from "./img/Shadow_Shaman_icon.webp";
+import Silencer from "./img/Silencer_icon.webp";
+import Skywrath_Mage from "./img/Skywrath_Mage_icon.webp";
+import Slardar from "./img/Slardar_icon.webp";
+import Slark from "./img/Slark_icon.webp";
+import Snapfire from "./img/Snapfire_icon.webp";
+import Sniper from "./img/Sniper_icon.webp";
+import Spectre from "./img/Spectre_icon.webp";
+import Spirit_Breaker from "./img/Spirit_Breaker_icon.webp";
+import Storm_Spirit from "./img/Storm_Spirit_icon.webp";
+import Sven from "./img/Sven_icon.webp";
+import Techies from "./img/Techies_icon.webp";
+import Templar_Assassin from "./img/Templar_Assassin_icon.webp";
+import Terrorblade from "./img/Terrorblade_icon.webp";
+import Tidehunter from "./img/Tidehunter_icon.webp";
+import Timbersaw from "./img/Timbersaw_icon.webp";
+import Tinker from "./img/Tinker_icon.webp";
+import Tiny from "./img/Tiny_icon.webp";
+import Treant_Protector from "./img/Treant_Protector_icon.webp";
+import Troll_Warlord from "./img/Troll_Warlord_icon.webp";
+import Tusk from "./img/Tusk_icon.webp";
+import Underlord from "./img/Underlord_icon.webp";
+import Undying from "./img/Undying_icon.webp";
+import Ursa from "./img/Ursa_icon.webp";
+import Vengeful_Spirit from "./img/Vengeful_Spirit_icon.webp";
+import Venomancer from "./img/Venomancer_icon.webp";
+import Viper from "./img/Viper_icon.webp";
+import Visage from "./img/Visage_icon.webp";
+import Void_Spirit from "./img/Void_Spirit_icon.webp";
+import Warlock from "./img/Warlock_icon.webp";
+import Weaver from "./img/Weaver_icon.webp";
+import Windranger from "./img/Windranger_icon.webp";
+import Winter_Wyvern from "./img/Winter_Wyvern_icon.webp";
+import Witch_Doctor from "./img/Witch_Doctor_icon.webp";
+import Wraith_King from "./img/Wraith_King_icon.webp";
+import Zeus from "./img/Zeus_icon.webp";
+
+export const heroes = [
+  {
+    img: abbadon,
+    name: "Abbadon",
+  },
+  {
+    img: alchemist,
+    name: "Alchemist",
+  },
+  {
+    img: Anti_Mage,
+    name: "Anti Mage",
+  },
+  {
+    img: ancient_apparition,
+    name: "Ancient Apparition",
+  },
+  {
+    img: Arc_Warden,
+    name: "Arc Warden",
+  },
+  {
+    img: Axe,
+    name: "Axe",
+  },
+  {
+    img: Bane,
+    name: "Bane",
+  },
+  {
+    img: Batrider,
+    name: "Batrider",
+  },
+  {
+    img: Beastmaster,
+    name: "Beastmaster",
+  },
+  {
+    img: Bloodseeker,
+    name: "Bloodseeker",
+  },
+  {
+    img: Bounty_Hunter,
+    name: "Bounty Hunter",
+  },
+  {
+    img: Brewmaster,
+    name: "Brewmaster",
+  },
+  {
+    img: Bristleback,
+    name: "Bristleback",
+  },
+  {
+    img: Broodmother,
+    name: "Broodmother",
+  },
+  {
+    img: Centaur_Warrunner,
+    name: "Centaur Warrunner",
+  },
+  {
+    img: Chaos_Knight,
+    name: "Chaos Knight",
+  },
+  {
+    img: Chen,
+    name: "Chen",
+  },
+  {
+    img: Clinkz,
+    name: "Clinkz",
+  },
+  {
+    img: Clockwerk,
+    name: "Clockwerk",
+  },
+  {
+    img: Crystal_Maiden,
+    name: "Crystal Maiden",
+  },
+  {
+    img: Dark_Seer,
+    name: "Dark Seer",
+  },
+  {
+    img: Dark_Willow,
+    name: "Dark Willow",
+  },
+  {
+    img: Dawnbreaker,
+    name: "Dawnbreaker",
+  },
+  {
+    img: Dazzle,
+    name: "Dazzle",
+  },
+  {
+    img: Death_Prophet,
+    name: "Death Prophet",
+  },
+  {
+    img: Disruptor,
+    name: "Disruptor",
+  },
+  {
+    img: Doom,
+    name: "Doom",
+  },
+  {
+    img: Dragon_Knight,
+    name: "Dragon Knight",
+  },
+  {
+    img: Drow_Ranger,
+    name: "Drow Ranger",
+  },
+  {
+    img: Earth_Spirit,
+    name: "Earth Spirit",
+  },
+  {
+    img: Earthshaker,
+    name: "Earthshaker",
+  },
+  {
+    img: Elder_Titan,
+    name: "Elder Titan",
+  },
+  {
+    img: Ember_Spirit,
+    name: "Ember Spirit",
+  },
+  {
+    img: Enchantress,
+    name: "Enchantress",
+  },
+  {
+    img: Enigma,
+    name: "Enigma",
+  },
+  {
+    img: Faceless_Void,
+    name: "Faceless Void",
+  },
+  {
+    img: Grimstroke,
+    name: "Grimstroke",
+  },
+  {
+    img: Gyrocopter,
+    name: "Gyrocopter",
+  },
+  {
+    img: Hoodwink,
+    name: "Hoodwink",
+  },
+  {
+    img: Huskar,
+    name: "Huskar",
+  },
+  {
+    img: Invoker,
+    name: "Invoker",
+  },
+  {
+    img: Io,
+    name: "Io",
+  },
+  {
+    img: Jakiro,
+    name: "Jakiro",
+  },
+  {
+    img: Juggernaut,
+    name: "Juggernaut",
+  },
+  {
+    img: Keeper_of_the_Light,
+    name: "Keeper of the Light",
+  },
+  {
+    img: Kunkka,
+    name: "Kunkka",
+  },
+  {
+    img: Legion_Commander,
+    name: "Legion Commander",
+  },
+  {
+    img: Leshrac,
+    name: "Leshrac",
+  },
+  {
+    img: Lich,
+    name: "Lich",
+  },
+  {
+    img: Lifestealer,
+    name: "Lifestealer",
+  },
+  {
+    img: Lina,
+    name: "Lina",
+  },
+  {
+    img: Lion,
+    name: "Lion",
+  },
+  {
+    img: Lone_Druid,
+    name: "Lone Druid",
+  },
+  {
+    img: Luna,
+    name: "Luna",
+  },
+  {
+    img: Lycan,
+    name: "Lycan",
+  },
+  {
+    img: Magnus,
+    name: "Magnus",
+  },
+  {
+    img: Marci,
+    name: "Marci",
+  },
+  {
+    img: Mars,
+    name: "Mars",
+  },
+  {
+    img: Medusa,
+    name: "Medusa",
+  },
+  {
+    img: Meepo,
+    name: "Meepo",
+  },
+  {
+    img: Mirana,
+    name: "Mirana",
+  },
+  {
+    img: Monkey_King,
+    name: "Monkey King",
+  },
+  {
+    img: Morphling,
+    name: "Morphling",
+  },
+  {
+    img: Naga_Siren,
+    name: "Naga Siren",
+  },
+  {
+    img: Natures_Prophet,
+    name: "Natures Prophet",
+  },
+  {
+    img: Necrophos,
+    name: "Necrophos",
+  },
+  {
+    img: Night_Stalker,
+    name: "Night Stalker",
+  },
+  {
+    img: Nyx_Assassin,
+    name: "Nyx Assassin",
+  },
+  {
+    img: Ogre_Magi,
+    name: "Ogre Magi",
+  },
+  {
+    img: Omniknight,
+    name: "Omniknight",
+  },
+  {
+    img: Oracle,
+    name: "Oracle",
+  },
+  {
+    img: Outworld_Destroyer,
+    name: "Outworld Destroyer",
+  },
+  {
+    img: Pangolier,
+    name: "Pangolier",
+  },
+  {
+    img: Phantom_Assassin,
+    name: "Phantom Assassin",
+  },
+  {
+    img: Phantom_Lancer,
+    name: "Phantom Lancer",
+  },
+  {
+    img: Phoenix,
+    name: "Phoenix",
+  },
+  {
+    img: Primal_Beast,
+    name: "Primal Beast",
+  },
+  {
+    img: Puck,
+    name: "Puck",
+  },
+  {
+    img: Pudge,
+    name: "Pudge",
+  },
+  {
+    img: Pugna,
+    name: "Pugna",
+  },
+  {
+    img: Queen_of_Pain,
+    name: "Queen of Pain",
+  },
+  {
+    img: Razor,
+    name: "Razor",
+  },
+  {
+    img: Riki,
+    name: "Riki",
+  },
+  {
+    img: Rubick,
+    name: "Rubick",
+  },
+  {
+    img: Sand_King,
+    name: "Sand King",
+  },
+  {
+    img: Shadow_Demon,
+    name: "Shadow Demon",
+  },
+  {
+    img: Shadow_Fiend,
+    name: "Shadow Fiend",
+  },
+  {
+    img: Shadow_Shaman,
+    name: "Shadow Shaman",
+  },
+  {
+    img: Silencer,
+    name: "Silencer",
+  },
+  {
+    img: Skywrath_Mage,
+    name: "Skywrath Mage",
+  },
+  {
+    img: Slardar,
+    name: "Slardar",
+  },
+  {
+    img: Slark,
+    name: "Slark",
+  },
+  {
+    img: Snapfire,
+    name: "Snapfire",
+  },
+  {
+    img: Sniper,
+    name: "Sniper",
+  },
+  {
+    img: Spectre,
+    name: "Spectre",
+  },
+  {
+    img: Spirit_Breaker,
+    name: "Spirit Breaker",
+  },
+  {
+    img: Storm_Spirit,
+    name: "Storm Spirit",
+  },
+  {
+    img: Sven,
+    name: "Sven",
+  },
+  {
+    img: Techies,
+    name: "Techies",
+  },
+  {
+    img: Templar_Assassin,
+    name: "Templar Assassin",
+  },
+  {
+    img: Terrorblade,
+    name: "Terrorblade",
+  },
+  {
+    img: Tidehunter,
+    name: "Tidehunter",
+  },
+  {
+    img: Timbersaw,
+    name: "Timbersaw",
+  },
+  {
+    img: Tinker,
+    name: "Tinker",
+  },
+  {
+    img: Tiny,
+    name: "Tiny",
+  },
+  {
+    img: Treant_Protector,
+    name: "Treant Protector",
+  },
+  {
+    img: Troll_Warlord,
+    name: "Troll Warlord",
+  },
+  {
+    img: Tusk,
+    name: "Tusk",
+  },
+  {
+    img: Underlord,
+    name: "Underlord",
+  },
+  {
+    img: Undying,
+    name: "Undying",
+  },
+  {
+    img: Ursa,
+    name: "Ursa",
+  },
+  {
+    img: Vengeful_Spirit,
+    name: "Vengeful Spirit",
+  },
+  {
+    img: Venomancer,
+    name: "Venomancer",
+  },
+  {
+    img: Viper,
+    name: "Viper",
+  },
+  {
+    img: Visage,
+    name: "Visage",
+  },
+  {
+    img: Void_Spirit,
+    name: "Void Spirit",
+  },
+  {
+    img: Warlock,
+    name: "Warlock",
+  },
+  {
+    img: Weaver,
+    name: "Weaver",
+  },
+  {
+    img: Windranger,
+    name: "Windranger",
+  },
+  {
+    img: Winter_Wyvern,
+    name: "Winter Wyvern",
+  },
+  {
+    img: Witch_Doctor,
+    name: "Witch Doctor",
+  },
+  {
+    img: Wraith_King,
+    name: "Wraith King",
+  },
+  {
+    img: Zeus,
+    name: "Zeus",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import ItalicFonts from "./ItalicFonts.jsx";
-
+import DreamTeam from "./complexComponent/DreamTeam";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    <ItalicFonts toolTip="Cursive" />
-
+    <DreamTeam />
   </React.StrictMode>
 );
