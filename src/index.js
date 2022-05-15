4daa2a77b1ce4330d93d62c143803350c9ce8c1d
aiakimov <akim.boroda@gmail.com>
2022-05-15 18:18:54 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AddName from "./AddName.tsx";
-
-
-import DreamTeam from "./complexComponent/DreamTeam";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-
-
-    <DreamTeam />
-
-
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
