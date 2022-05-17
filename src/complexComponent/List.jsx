3977e2d130f5b2ebcfd5265765848a1d7ad5505c
aiakimov <akim.boroda@gmail.com>
2022-05-17 16:31:31 +0300

    Task-22.1

--- src/complexComponent/List.jsx
+++ src/complexComponent/List.jsx
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { heroes } from "./herous.jsx";
+import { setToLocalStorage, getFromLocalStorage } from "./localstorage.jsx";
 
-function List({ onClickHandler }) {
-  return heroes.map((el, index) => {
+function List({ setAmountCheckedCards }) {
+  const [innerAmountCheckedCards, setInnerAmountCheckedCards] = useState(0);
+  const [selectedHeroes, setSelectedHeroes] = useState(
+    heroes.map(() => {
+      return false;
+    })
+  );
+  useEffect(() => {
+    const amount = selectedHeroes.filter((el) => el === true).length;
+    setInnerAmountCheckedCards(amount);
+    setAmountCheckedCards(amount);
+  }, [selectedHeroes]);
+
+  useEffect(() => {
+    if (getFromLocalStorage()) {
+      setSelectedHeroes(getFromLocalStorage());
+    }
+  }, []);
+
+  const onSelectHero = (index) => {
+    const copySelectedHeroes = [...selectedHeroes];
+    if (innerAmountCheckedCards < 5) {
+      copySelectedHeroes[index] = !copySelectedHeroes[index];
+      setSelectedHeroes(copySelectedHeroes);
+      setToLocalStorage(copySelectedHeroes);
+    } else {
+      if (copySelectedHeroes[index] === true) {
+        copySelectedHeroes[index] = !copySelectedHeroes[index];
+        setSelectedHeroes(copySelectedHeroes);
+        setToLocalStorage(copySelectedHeroes);
+      }
+    }
+  };
+
+  return heroes.map((hero, index) => {
     return (
       <li
-        className="hero-card"
-        key={index}
+        className={
+          selectedHeroes[index] ? "hero-card checked-card" : "hero-card"
+        }
+        key={hero.name}
         onClick={() => {
-          onClickHandler(index);
+          onSelectHero(index);
         }}
       >
-        <img src={el.img} alt="" />
-        <span>{el.name}</span>
+        <img src={hero.img} alt="" />
+        <span>{hero.name}</span>
       </li>
     );
   });
