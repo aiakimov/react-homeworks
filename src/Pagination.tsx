649ac3aa56745dc23a0c239b593681894e332dfb
aiakimov <akim.boroda@gmail.com>
2022-05-17 20:42:43 +0300

    completed Task-29

---
+++ src/Pagination.tsx
@@ -0,0 +1,67 @@
+import React, { FC, useEffect, useState } from "react";
+import "./Pagination.css";
+import axios from "axios";
+
+const Pagination: FC = () => {
+  const API: string = "https://jsonplaceholder.typicode.com/";
+  const pages: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const [photos, setPhotos] = useState<PhotoObj[]>([]);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  interface PhotoObj {
+    albumId: number;
+    id: number;
+    thumbnailUrl: string;
+    title: string;
+    url: string;
+  }
+
+  const getPhotos = async () => {
+    const responce = await axios.get(
+      API + `photos?_page=${pageNumber}&_limit=24`
+    );
+    setPhotos(responce?.data);
+  };
+
+  const onclick = (btnId: number) => {
+    setPageNumber(btnId);
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, [pageNumber]);
+
+  return (
+    <>
+      <div className="contant">
+        {photos.map((item) => {
+          return (
+            <div key={item.url}>
+              <img src={item.url} alt="" />
+              <p>{item.title}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="pagination-bar">
+        {pages.map((item) => {
+          return (
+            <button
+              onClick={() => {
+                onclick(item);
+              }}
+              key={item}
+              className="pagination-btn"
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Pagination from "./Pagination.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Pagination />
+  </React.StrictMode>
+);
