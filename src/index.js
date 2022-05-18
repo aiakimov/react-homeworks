54c3016d0fa87cedca9b1fb9e26d64c24ec80b02
aiakimov <akim.boroda@gmail.com>
2022-05-18 11:06:40 +0300

    completed Task-24

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import TimePicker from "./TimePicker.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <TimePicker />
+  </React.StrictMode>
+);
