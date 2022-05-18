cb7ea6e63144f4a0cb8a450b8df253e29f7c69f3
aiakimov <akim.boroda@gmail.com>
2022-05-18 11:33:55 +0300

    completed Task-Checkout-Main

---
+++ src/AddName.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from "react";
+import "./AddName.css";
+
+const AddName: FC = () => {
+  const names = [
+    "James",
+    "Oliver",
+    "Benjamin",
+    "Emma",
+    "Isabella",
+    "Sophia",
+    "Charlotte",
+    "Mia",
+    "Lucas",
+    "Mason",
+    "Evelyn",
+    "Elijah",
+    "Ava",
+  ];
+  const [people, setPeople] = useState<string[]>(["Jonh", "Bob", "Marlyn"]);
+
+  const addNameToList = () => {
+    const index = Math.floor(Math.random() * 13);
+    const newArray = [...people, names[index]];
+    setPeople(newArray);
+  };
+
+  return (
+    <div className="wrapper">
+      <ul>
+        {people.map((name) => {
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+      <button onClick={addNameToList}>Add Name</button>
+    </div>
+  );
+};
+export default AddName;
