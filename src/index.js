540b135beb33862074aa5178289a496b073d0bcc
aiakimov <akim.boroda@gmail.com>
2022-05-20 21:06:49 +0300

    completed Task-27

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import NavBar from "./Components/Router/NavBar";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
